921c70838e5a07b4b72cf5367cafe8318e73fb40
TRB-Exe <72357112+TRB-Exe@users.noreply.github.com>
2023-11-18 12:17:21 +0300

    Create maintenance.php

---
+++ maintenance.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html_lang="en">
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>sorry</title>
+<style>
+html { color-scheme: light dark; }
+body { width: 35em; margin: 0 auto;
+font-family: Arial; }
+                              </style>
+                              </head>
+                              <body>
+                              <h1>Under maintenance</h1>
+                              <p>This site is currently unavailable due to maintenance. Visit this site later.</p>
+                                
+                                        </a></center>
+                                        <p><em>amogus sus</em></p>
+                                        </body>
+                                        </html>
