ce73d82bb51c5711c0b1073b15f98a8a13a6b72b
TRB-Exe <72357112+TRB-Exe@users.noreply.github.com>
2023-11-18 12:18:53 +0300

    Create index.php

---
+++ admin/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html_lang="en">
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Admin panel</title>
+<style>
+html { color-scheme: light dark; }
+body { width: 35em; margin: 0 auto;
+font-family: Arial; }
+                              </style>
+                              </head>
+                              <body>
+                              <h1>Example Admin Panel</h1>
+                              <a href="maintenance_manage.php">Manage maintenance mode</a>
+                                
+                                        </a></center>
+                                        <p><em>amogus sus</em></p>
+                                        </body>
+                                        </html>
